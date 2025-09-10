76ef4df5a00dfca7c7975a23989dca2d3e2a60f0
nitta <nitta@DESKTOP-IE2HGGK>
2025-09-11 00:31:19 +0700

    Добавьте файлы проекта.

---
+++ laba1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        double a = 10;
+        double b = 8;
+        double L = 20;
+        double r = 3;
+        double l = 15;
+
+        double v1 = a * b * L;
+        double v2 = 3.14 * r * r * l;
+        double waste = v1 - v2;
+        double percent = (waste / v1) * 100;
+
+        Console.WriteLine("Объем исходного: " + v1);
+        Console.WriteLine("Объем выточенного: " + v2);
+        Console.WriteLine("Отходы: " + waste);
+        Console.WriteLine("Процент: " + percent + "%");
+    }
+}
